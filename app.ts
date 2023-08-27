5726412a1b942b7085bba9bb1b54a9810ea46267
ihccenen <wirkz01@gmail.com>
2023-08-27 15:05:21 -0300

    added user route and errors middleware

--- app.ts
+++ app.ts
@@ -5,7 +5,9 @@ import helmet from 'helmet';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
+import { notFound, errorHandler } from './middleware/errorMiddleware';
 import connectDB from './config/db';
+import userRouter from './routes/user';
 
 const PORT = process.env.PORT || 5000;
 
@@ -19,5 +21,10 @@ app.use(cookieParser());
 app.use(helmet());
 app.use(cors());
 app.use(morgan('dev'));
+
+app.use('/api/v1/users', userRouter);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
