3ec6753af8b19ec8a0cd450e7f27cda6e35f8bd0
ihccenen <wirkz01@gmail.com>
2023-08-31 21:05:33 -0300

    added client and cms origins

--- app.ts
+++ app.ts
@@ -16,11 +16,24 @@ connectDB();
 
 const app = express();
 
+const whitelist = [process.env.CLIENT_URL, process.env.CMS_URL];
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(helmet());
-app.use(cors({ origin: process.env.ORIGIN_URL, credentials: true }));
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true,
+  })
+);
 app.use(morgan('dev'));
 
 app.use('/api/v1/users', userRouter);
